60d95d3b83d0d8eb999e6ef63aed4a6eedfc7a18
ABDish <abdish@Abhinavs-MacBook-Air.local>
2023-02-10 18:34:45 +0530

    initial commit

--- index.js
+++ index.js
@@ -8,30 +8,28 @@ import {
 
 export default class new_react_360_app extends React.Component {
   render() {
-    return (
-     
-    );
+    return ();
   }
 };
 
-// const styles = StyleSheet.create({
-//   panel: {
-//     // Fill the entire surface
-//     width: 1000,
-//     height: 600,
-//     backgroundColor: 'rgba(255, 255, 255, 0.4)',
-//     justifyContent: 'center',
-//     alignItems: 'center',
-//   },
-//   greetingBox: {
-//     padding: 20,
-//     backgroundColor: '#000000',
-//     borderColor: '#639dda',
-//     borderWidth: 2,
-//   },
-//   greeting: {
-//     fontSize: 30,
-//   },
-// });
+const styles = StyleSheet.create({
+  panel: {
+    // Fill the entire surface
+    width: 1000,
+    height: 600,
+    backgroundColor: 'rgba(255, 255, 255, 0.4)',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  greetingBox: {
+    padding: 20,
+    backgroundColor: '#000000',
+    borderColor: '#639dda',
+    borderWidth: 2,
+  },
+  greeting: {
+    fontSize: 30,
+  },
+});
 
 AppRegistry.registerComponent('new_react_360_app', () => new_react_360_app);
